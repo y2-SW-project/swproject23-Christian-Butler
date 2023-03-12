e1371311273b994385f1613aa787b10d4f434d61
Christian Butler <N00211989@iadt.ie>
2023-03-12 20:03:32 +0000

    created the artist factory with various attributes to seed the artist table with fake data

---
+++ database/factories/ArtistFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ArtistFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "first_name"->$this->faker->name,
+            "last_name"->$this->faker->name,
+            "genre"->$this->faker->word,
+            "date"->$this->$faker->date,
+        ];
+    }
+}
