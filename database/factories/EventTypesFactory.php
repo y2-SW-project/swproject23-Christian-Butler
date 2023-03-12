3a10b0af62670a9e59d9455f8ba50bde108bd4b9
Christian Butler <N00211989@iadt.ie>
2023-03-12 20:06:59 +0000

    created the event type factory to seed the cart table with fake data

---
+++ database/factories/EventTypesFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\event_types>
+ */
+class EventTypesFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "event_name"->$this->faker->name,
+            "location"->$this->faker->text,
+        ];
+    }
+}
