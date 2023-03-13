bcb902d438e9316ebc82efcd515d4dc3d7be8032
Christian Butler <N00211989@iadt.ie>
2023-03-13 21:38:30 +0000

    added the various seeder classes to run the seeders to populate the tables with fake data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(ArtistSeeder::class);
+        $this->call(VenueSeeder::class);
         $this->call(EventsSeeder::class);
         $this->call(EventTypesSeeder::class);
-        $this->call(VenueSeeder::class);
     }
 }
